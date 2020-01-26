3ffb2abcda886f78e81869be2992d99dbc51631d
Walter Baleco <Wbaleco@hotmail.com>
2020-01-26 12:40:02 -0300

    MONGODB - Criar Schema e collections e inserir registros

--- app.js
+++ app.js
@@ -8,3 +8,24 @@ mongoose.connect('mongodb://localhost/gerenciadorfinanceiro', {
 }).catch((err) => {
     console.log("Erro ao tentar conectar" + err);
 });
+
+const PagamentoSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    valor: {
+        type: Number,
+        require: true
+    }
+});
+
+var Pagamento = mongoose.model('Pagamentos', PagamentoSchema);
+new Pagamento({
+    nome: "Internet",
+    valor: 150
+}).save().then(() => {
+    console.log("Cadastro realizado com sucesso");
+}).catch((err) => {
+    console.log("Erro ao tentar cadastrar" + err);
+})
